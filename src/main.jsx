2a29b8136c1258030f5de216ef574d6398a71496
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-02 22:33:50 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/food_diary">
+        <BrowserRouter basename="/fs-84-react-node-Ne1rem">
           <App />
           <ToastContainer />
         </BrowserRouter>
